e5d97992affb9cdc61991ccb3d5827d7fc4fb204
김명진 <hunkun746@cu.ac.kr>
2019-11-03 13:18:44 +0900

    19126732

---
+++ 소스1.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	double a;
+	double b = 2.204623;
+
+	printf("무게(kg)입력: ");
+	scanf("%lf", &a);
+
+	printf("%.3f파운드", a * b);
+}
